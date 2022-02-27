a07207d8376b46d7f5536c5cf020f2c9ac46caec
Al Mohite <98574955+almohite2000@users.noreply.github.com>
2022-02-27 21:02:23 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(".owl-carousel").owlCarousel({
+        margin: 10,
+        loop: true,
+        autoplay: true,
+        autoplaytimeout: 2000, //2000 ms = 2 sec
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            },
+        }
+  });
